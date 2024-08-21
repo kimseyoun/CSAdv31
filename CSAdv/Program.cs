f523cbdbb45c930d597ef714cd550926d4171deb
kimseyoun <103114362+kimseyoun@users.noreply.github.com>
2024-08-21 09:02:08 +0900

    제네릭 실습1

---
+++ CSAdv/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAdv
+{
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wantedString = new Wanted<string>("string");
+            Wanted<int> wantedint = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedint.Value);
+            Console.WriteLine(wantedDouble.Value);
+        }
+    }
+}
