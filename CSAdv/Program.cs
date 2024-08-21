9b3bf3df4f19fd321ba9fae0f2ad56947ceedc61
kimseyoun <103114362+kimseyoun@users.noreply.github.com>
2024-08-21 09:03:44 +0900

    제네릭실습2

--- CSAdv/Program.cs
+++ CSAdv/Program.cs
@@ -14,6 +14,12 @@ namespace CSAdv
             this.Value = value;
         }
     }
+    class WantedTest<T, U>
+        where T : IComparable
+        where U : IComparable, IDisposable
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
